0ee07d146d176506a0868856d145764600dfd3d6
cs <2900455274@qq.com>
2020-06-08 14:37:01 +0800

    初始化项目

---
+++ react-base-demo/src/index.js
@@ -0,0 +1,13 @@
+// document.write('aaaaaaaaaa');
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+function App(){
+    return(
+        <div>hhhhhhhhh??????????</div>
+    )
+}
+ReactDOM.render(<App />,document.getElementById('root'))
+
